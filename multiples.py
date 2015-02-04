e09fc64ea3b9aa4481896e2fef64ccb332d412c1
heroms01 <heroms01@gmail.com>
2015-02-04 17:22:39 +0900

    다시 수정
    잘못 풀음.

--- multiples.py
+++ multiples.py
@@ -5,13 +5,11 @@
 # 1000미만의 자연수에서 3,5의 배수의 총합을 구하라.
 
 limit = 1000
-# 3의 배수 구하기
-x3 = [n*3 for n in xrange(limit) if n*3 < limit]
-# 5의 배수 구하기
-x5 = [n*5 for n in xrange(limit) if n*5 < limit]
 
-# 각 리스트 합계 더하기
-print(sum(x3)+sum(x5))
+x = [n for n in xrange(limit) if n % 3== 0 or n % 5== 0]
+
+# 리스트 합계 더하기
+print(sum(x))
 
 # for i in xrange(limit):
 # 	temp = 3*i
